e29b46e9da75dcd9a603171965bb9de68e830c2d
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-07-20 20:49:45 +0500

    Chore: removed pass validation

--- components/signup/registrationController.js
+++ components/signup/registrationController.js
@@ -12,45 +12,30 @@ const createUser = async (req, res) => {
             ]
         );
         if (alreadyregistered.rowCount == 0) {
-
-            if (
-                userDetail.password == userDetail.cpassword &&
-                userDetail.password.length >= 8 &&
-                /[!@#$%^&*(),.?":{}|<>]/g.test(userDetail.password)
-            ) {
-
-                const salt = await bcrypt.genSalt()
-                const hashedPassword = await bcrypt.hash(userDetail.password, salt);
-                await pool.query(
-                    `INSERT INTO users (first_name, last_name, email, password, gender, phone_no, user_type)
+            const salt = await bcrypt.genSalt()
+            const hashedPassword = await bcrypt.hash(userDetail.password, salt);
+            await pool.query(
+                `INSERT INTO users (first_name, last_name, email, password, gender, phone_no, user_type)
                     VALUES ($1, $2, $3, $4, $5, $6,$7);`,
-                    [
-                        userDetail.first_name,
-                        userDetail.last_name,
-                        userDetail.email,
-                        hashedPassword,
-                        userDetail.gender,
-                        userDetail.phone_no,
-                        userDetail.user_type
-                        // userDetail.city,
-                        // userDetail.town,
-                        // userDetail.street,
-                        // userDetail.house_no,
-                        // userDetail.postal_code,
-                        // userDetail.latitude,
-                        // userDetail.longitude
+                [
+                    userDetail.first_name,
+                    userDetail.last_name,
+                    userDetail.email,
+                    hashedPassword,
+                    userDetail.gender,
+                    userDetail.phone_no,
+                    userDetail.user_type
+                    // userDetail.city,
+                    // userDetail.town,
+                    // userDetail.street,
+                    // userDetail.house_no,
+                    // userDetail.postal_code,
+                    // userDetail.latitude,
+                    // userDetail.longitude
 
-                    ]
-                );
-                res.send("User registered successfully");
-            }
-            else {
-                res
-                    .status(400)
-                    .json({
-                        error: "Password and confirm password do not match, password is less than 8 characters, or password does not contain a special character.",
-                    });
-            }
+                ]
+            );
+            res.send("User registered successfully");
 
         }
         else {
