b3fd9565116d55bc0bcb8c96f9e3cd669d5f4683
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-07-24 15:57:17 +0500

    new db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "anoosh11",
-    host: "localhost",
+    user: "jwbyataieyzzde",
+    password: "8e206c5dd35c8ccc89999ddb5c79cfbf278b528107726679e3430db9e74e6a26",
+    host: "ec2-3-234-204-26.compute-1.amazonaws.com",
     port: 5432,
-    database: "gmfree"
+    database: "dbalnvevh40ggr"
 });
 
 module.exports = pool;
