a9726187e94f943b4ef405927b0e733e4f2f14d1
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-08-29 18:33:48 +0500

    chore: name changed

--- routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const authControllers = require('../controllers/auth/index');
-const router = express.Router()
-//ROUTES
-router.post('/sign-up', authControllers.createUser)
-router.post('/log-out', authControllers.logoutUser);
-router.post('/log-in', authControllers.loginUser)
-module.exports = router
