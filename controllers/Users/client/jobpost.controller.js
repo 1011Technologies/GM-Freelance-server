5d4f19b9e4b7bed847fc199b6c41f277aefc651f
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-08-29 19:35:31 +0500

    feat: job post

--- controllers/Users/client/jobpost.controller.js
+++ controllers/Users/client/jobpost.controller.js
@@ -5,7 +5,7 @@ const postJob = async (req, res) => {
     try {
         const { client_id, job_title, job_catagory, from_date, till_date, duration, description, budget } = req.body;
 
-        if (rating && distance && response_rate && response_time && days_available && hourly_rate) {
+        if (client_id && job_title && job_catagory && from_date && till_date && duration && description && budget) {
             await pool.query('BEGIN');
             await pool.query(
                 'INSERT INTO job (client_id, job_title, job_catagory, from_date, till_date, duration, description, budget) VALUES ($1, $2, $3, $4, $5, $6, $7,$8)',
@@ -15,7 +15,7 @@ const postJob = async (req, res) => {
 
             res.status(200).json({ message: 'Job Posted' });
         } else {
-            res.status(400).json({ error: 'Enter correct details' });
+            res.status(400);
         }
     } catch (error) {
         await pool.query('ROLLBACK');
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const freelancerRoute= require('./freelancer.route')
 
 const router = express();
 router.use('/auth', authRoute);
-router.use('/users', userRoute);
+router.use('/user', userRoute);
 router.use('/chat', chatRoute);
 router.use('/client', clientRoute);
 router.use('/freelancer', freelancerRoute);
