35db2cd2e5bb4ffda4876c11420ce9c6af99c431
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-08-30 15:47:50 +0500

    chore: folder structure change

--- controllers/Update/index.js
+++
@@ -1,9 +0,0 @@
-const updatePassController = require('./updatePassword.controller');
-const updateDetailsController = require('./updateUserDetails.controller');
-const updateProfilePictureController = require('./updateProfilePicture.controller');
-
-module.exports = {
-    updatePass: updatePassController.updatePassword,
-    updateUserDetail: updateDetailsController.updateDetail,
-    updateProfilePic: updateProfilePictureController.uploadProfilePicture,
-};
--- controllers/Users/client/index.js
+++
@@ -1,8 +0,0 @@
-const clientController = require('./client.controller');
-const jobPostController = require('./jobpost.controller');
-
-module.exports = {
-    getClientData: clientController.getClientData,
-    saveClientData: clientController.saveClientData,
-    postJob:jobPostController.postJob
-};
