3f3fea964afde45bfb0e7b6e226c1e344e2f7efe
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-09-04 19:16:42 +0500

    chore : extra code removed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //SERVER FILE
 require('dotenv').config();
 const express = require("express");
-const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require("cors");
 const cookieParser = require('cookie-parser');
@@ -10,58 +9,41 @@ const index = require('./routes');
 const port = process.env.PORT || 5000;
 
 const app = express();
-const allowedOrigins = [
-    'https://git.heroku.com/gigmate.git',
-    'http://localhost:3000',
-];
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 // middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-    cors({
-        origin: allowedOrigins,
-        methods: ["GET", "POST", "PUT"],
-        credentials: true,
-    }));
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-    session({
-        key: "user_id",
-        secret: 'gm_gigmeta',
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            expires: 60 * 60 * 72
-        }
-    })
-);
-const herokuApiKey = process.env.HEROKU_API_KEY;
-
 //ROUTES
 app.use('/api', index);
 const server = app.listen(port, () => {
     console.log('listening for requests on port', process.env.PORT)
 })
 
-//SOCKETS
-const { validateToken } = require('./utils/JWT');
-const io = require('socket.io')(server);
-io.use(async (socket, next) => {
-    try {
-        const token = socket.handshake.query.token
-        const payload = await validateToken(token);
-        socket.userid = payload.userid;
-        next();
-    } catch (error) {
-        console.error("Socket authentication error:", error);
-        socket.disconnect(true);
-    }
-})
+// //SOCKETS
+// const { validateToken } = require('./utils/JWT');
+// const io = require('socket.io')(server);
+// io.use(async (socket, next) => {
+//     try {
+//         const token = socket.handshake.query.token
+//         const payload = await validateToken(token);
+//         socket.userid = payload.userid;
+//         next();
+//     } catch (error) {
+//         console.error("Socket authentication error:", error);
+//         socket.disconnect(true);
+//     }
+// })
 
-io.on('connection', (socket) => {
-    console.log("Connected:" + socket.userid)
-    socket.on('disconnect', () => {
-        console.log("Disconnected:" + socket.userid)
+// io.on('connection', (socket) => {
+//     console.log("Connected:" + socket.userid)
+//     socket.on('disconnect', () => {
+//         console.log("Disconnected:" + socket.userid)
 
-    })
-})
+//     })
+// })
