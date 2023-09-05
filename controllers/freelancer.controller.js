097362a212ea355b157b7f5858d945a777773cc8
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-09-05 21:10:14 +0500

    feat: proposal submission

--- controllers/freelancer.controller.js
+++ controllers/freelancer.controller.js
@@ -1,32 +1,52 @@
 const pool = require('../db');
-
-
 const getFreelancerData = async (req, res) => {
     try {
-        const { rating, distance, response_rate, response_time, days_available, hourly_rate } = req.body;
-
-        if (rating && distance && response_rate && response_time && days_available && hourly_rate) {
-            const sessionUserId = req.session.user.user_id;
-
-            await pool.query('BEGIN');
-            await pool.query(
-                'INSERT INTO freelancer (user_id, rating, distance, response_rate, response_time, days_available, hourly_rate) VALUES ($1, $2, $3, $4, $5, $6, $7)',
-                [sessionUserId, rating, distance, response_rate, response_time, days_available, hourly_rate]
-            );
+        await pool.query('BEGIN');
+        const result = await pool.query(
+            'Select * from freelancer where user_id=$1',
+            [req.user]
+        );
+        if (result.rows.length > 0) {
+            const freelancerDetails = result.rows[0];
             await pool.query('COMMIT');
-
-            res.status(200).json({ message: 'Freelancer detail inserted' });
-        } else {
-            res.status(400).json({ error: 'Enter correct details' });
+            return res.status(200).json(freelancerDetails);
         }
     } catch (error) {
         await pool.query('ROLLBACK');
-
         console.error(error.message);
         res.status(500).json({ error: 'Server error' });
     }
 };
 
+const submitProposal = async (req, res) => {
+    try {
+        if (!req.file || !req.file.filename) {
+            return res.status(400).json({ error: "File not provided" });
+        }
+        const fileName = req.file.filename;
+
+        const { freelancer_id, job_id, proposed_duration, proposed_price, cover_letter } = req.body;
+        // const attachmentUrl = `https://gmfree-server-644f0950f6dd.herokuapp.com/api/user/get-profile-picture/${fileName}`
+        if (freelancer_id && job_id && proposed_duration && proposed_price && cover_letter) {
+            await pool.query('BEGIN');
+            await pool.query(
+                `INSERT INTO proposal (freelancer_id, job_id,proposed_duration, proposed_price, cover_letter, attachment) VALUES ($1, $2, $3, $4, $5, $6);`,
+                [freelancer_id, job_id, proposed_duration, proposed_price, cover_letter, attachmentUrl]
+            );
+            await pool.query('COMMIT');
+            res.status(200).json({ message: 'Proposal Submitted' });
+        } else {
+            res.status(400).json({ error: 'Proposel Submission declined.' });
+        }
+
+    } catch (error) {
+        await pool.query('ROLLBACK');
+        console.error(error.message);
+        res.status(500).json({ error: 'Server error' });
+    }
+
+};
+
 module.exports = {
-    getFreelancerData
+    getFreelancerData, submitProposal
 }
--- routes/freelancer.route.js
+++ routes/freelancer.route.js
@@ -4,6 +4,22 @@ const path = require("path");
 const freelancerController = require('../controllers/freelancer.controller');
 const { validateToken } = require('../utils/JWT');
 const router = express.Router();
+const attachmentPath = path.join(__dirname, '../uploads/proposal');
+router.use('/attachment', express.static(attachmentPath));
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, attachmentPath)
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 },
+});
+router.get('/get-freelancer-data', validateToken, freelancerController.getFreelancerData);
 
-router.get('/get-freelancer-data', validateToken, freelancerController.getFreelancerData)
-module.exports = router
+router.post("/submit-proposal", validateToken, upload.single('proposalAttachment'), freelancerController.submitProposal);
+
+module.exports = router;
