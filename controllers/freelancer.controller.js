9dd85599279a81c665ae7db3152c9af6ed7f103d
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-09-06 19:53:43 +0500

    chore: submit proposal

--- controllers/freelancer.controller.js
+++ controllers/freelancer.controller.js
@@ -21,30 +21,27 @@ const getFreelancerData = async (req, res) => {
 
 const submitProposal = async (req, res) => {
     try {
-        if (!req.file || !req.file.filename) {
-            return res.status(400).json({ error: "File not provided" });
-        }
-        const fileName = req.file.filename;
+        const fileName = req.file?.filename;
 
         const { freelancer_id, job_id, proposed_duration, proposed_price, cover_letter } = req.body;
-        const attachmentUrl = `https://gmfree-server-644f0950f6dd.herokuapp.com/api/user/get-profile-picture/${fileName}`
+        const attachmentUrl = fileName ? `https://gmfree-server-644f0950f6dd.herokuapp.com/api/user/get-profile-picture/${fileName}` : null;
         if (freelancer_id && job_id && proposed_duration && proposed_price && cover_letter) {
             await pool.query('BEGIN');
             await pool.query(
-                `INSERT INTO proposal (freelancer_id, job_id,proposed_duration, proposed_price, cover_letter, attachment) VALUES ($1, $2, $3, $4, $5, $6);`,
+                `INSERT INTO proposal (freelancer_id, job_id, proposed_duration, proposed_price, cover_letter, attachment) VALUES ($1, $2, $3, $4, $5, $6);`,
                 [freelancer_id, job_id, proposed_duration, proposed_price, cover_letter, attachmentUrl]
             );
             await pool.query('COMMIT');
             res.status(200).json({ message: 'Proposal Submitted' });
         } else {
-            res.status(400).json({ error: 'Proposel Submission declined.' });
+            res.status(400).json({ error: 'Proposal Submission declined.' });
         }
-
     } catch (error) {
         await pool.query('ROLLBACK');
         console.error(error.message);
         res.status(500).json({ error: 'Server error' });
     }
+
 
 };
 
