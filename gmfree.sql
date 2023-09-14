0d53aaf9acfb3250f38daf220ae56083abb5726c
Muhammad Anoosh <muhammadanoosh.ma@gmail.com>
2023-09-14 21:01:14 +0500

    chore: updated sql

--- gmfree.sql
+++ gmfree.sql
@@ -149,3 +149,9 @@ CREATE TABLE bookmark (
     freelancer_id INT REFERENCES freelancer(freelancer_id) ON DELETE SET NULL,
     post_time TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE recently_viewed (
+    recently_viewed_id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES client(client_id) ON DELETE SET NULL,
+    freelancer_id INT REFERENCES freelancer(freelancer_id) ON DELETE SET NULL
+);
